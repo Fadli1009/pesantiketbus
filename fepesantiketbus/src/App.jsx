11e600c6026b624a56e002cb3f95d7812878b5e2
Fadli1009 <argyfa3@gmail.com>
2025-09-21 12:59:17 +0700

    install lib

--- fepesantiketbus/src/App.jsx
+++ fepesantiketbus/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='bg-green-100'>
+      <Button>pencet saya</Button>
+      <div className='flex'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
